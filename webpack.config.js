d02fd9e9c7aa6f5cea074ea9bf01d6194d47fc78
Subash_Chandra <s.chandra106@gmail.com>
2023-07-31 06:46:32 +0530

    added webpack setup

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  devServer: {
+    static: "dist",
+    port: 8000,
+  },
+  module: {
+    rules: [{ test: /\.css$/, use: ["style-loader", "css-loader"] }],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, "index.html"),
+      inject: true,
+    }),
+  ],
+};
